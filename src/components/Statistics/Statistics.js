9d61c84bcf8b7dbf06df4978140ec7323f0f325e
Veronika Riaboshlyk <riaboshlyk.veronika@gmail.com>
2021-12-22 21:15:56 +0100

    add title

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,7 +3,7 @@ import s from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => (
   <section className={s.statistics}>
-    <h2 className={s.title}>{title}</h2>
+    {title && <h2 className={s.statistics}>{title}</h2>}
     <ul className={s.stat_list}>
       {stats.map(stat => (
         <li className={s.item} key={stat.id}>
